27cd7ad0dd340a41d4f8fc1186458b7ab38f60b3
kartikey134 <kartikeyjaiswal31@gmail.com>
2021-08-17 17:54:58 +0530

    Completed Teams Model

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Team
+from django.utils.html import format_html
 
 # Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.photo.url))
+
+    thumbnail.short_description = 'photo';
+
+    list_display = ('id', 'thumbnail', 'firstName', 'designation', 'createdDate')
+    list_display_links = ('id', 'firstName')
+
+    search_fields = ('firstName', 'lastName', 'designation')
+    list_filter = ('designation',)
+
+admin.site.register(Team, TeamAdmin)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    facebookLink = models.URLField(max_length=100)
+    twitterLink = models.URLField(max_length=100)
+    googlePlusLink = models.URLField(max_length=100)
+    createdDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.firstName
